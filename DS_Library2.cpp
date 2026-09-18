267521b4611adf189dd63d94984f775b13b0a4f9
Shivani Ganesh Malkauthekar <shivanimal25@gmail.com>
2026-09-18 12:14:06 +0530

    Add files via upload

---
+++ DS_Library2.cpp
@@ -0,0 +1,31 @@
+//Search for a Book:
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int book[5];
+   int searchID;
+
+    cout << "Enter 5 Book IDs:\n";
+
+    for (int i = 0; i < 5; i++)
+    {
+       cin >> book[i];
+    }
+    cout << "\nEnter Book ID to search: ";
+    cin >> searchID;
+
+    for (int i = 0; i < 5; i++)
+    {
+       if (book[i] == searchID)
+       {
+            cout << "Book Found!";
+            return 0;
+       }
+   }
+    cout << "Book Not Found!";
+
+    return 0;
+}
